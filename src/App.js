04dcf685efe035cb0b0451256dc2306b82f5ada4
Mohammad-Zubair <m.zubair1138@gmail.com>
2022-06-03 06:55:07 +0500

    #comment applay useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Expenses from './components/Expenses/Expenses'
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <div className="container">
       <h1>Let's Get Started!</h1>
+      <NewExpense/>
       <Expenses/> 
       </div>
     </div>
--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -1,17 +1,24 @@
+import React, {useState} from 'react';
 import "./Expenseitem.css";
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const Expenseitem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("Updated!");
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}  />
             <div className="expense-item__description">
-                <div><h2>{props.title}</h2></div>
+                <div><h2>{title}</h2></div>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button className="btn btn-dark ms-1" onClick={}>Change Title</button>
+            <button className="btn btn-dark ms-1" onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
